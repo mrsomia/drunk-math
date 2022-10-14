813b9c3b24c11ce4fc6678d86fdfa24ac5fc6152
Sachin Somia <sachinsomia@gmail.com>
2022-10-14 14:54:35 +0100

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
+        <h1 className="text-5xl font-extrabold leading-normal text-gray-700 md:text-[5rem]">
+            Drunk Math
+        </h1>
+      </main>
+    </>
+  );
+};
+
+export default Home;
+
