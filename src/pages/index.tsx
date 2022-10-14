f927f16dd2fe5168f38994fd072a0c8d41465ce9
Sachin Somia <sachinsomia@gmail.com>
2022-10-14 18:03:48 +0100

    Adds BAC calc and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,29 @@
 import type { NextPage } from "next";
 import { useState } from "react";
+import { calcAlcoholInGrams, calcBAC } from "../utils";
 
 const Home: NextPage = () => {
-  const [isMale, setIsMale] = useState<null | boolean>(null)
-  const [weight, setWeight] = useState<null | number>(null)
+  const [isMale, setIsMale] = useState<null | boolean>(null);
+  const [weight, setWeight] = useState<null | number>(null);
+  const [ml, setMl] = useState<null | number>(null);
+  const [abv, setAbv] = useState<null | number>(null);
+  const [time, setTime] = useState<null | number>(null);
 
   const handleWeightChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setWeight(Number(e.target.value))
-  }
+    setWeight(Number(e.target.value));
+  };
+
+  const handleMlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMl(Number(e.target.value));
+  };
+
+  const handleAbvChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAbv(Number(e.target.value));
+  };
+
+  const handleTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTime(Number(e.target.value));
+  };
 
   return (
     <>
@@ -15,43 +31,108 @@ const Home: NextPage = () => {
         <h1 className="text-5xl font-extrabold leading-normal text-gray-700 md:text-[5rem]">
           Drunk Math
         </h1>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <fieldset>
-            <legend>Gender</legend>
-            <div>
+        <form className="grid grid-cols-2 gap-6 m-4 " onSubmit={(e) => e.preventDefault()}>
+          <div className="flex flex-col">
+
+            <fieldset className="p-2">
+              <legend className="text-center font-medium py-3">Gender</legend>
+              <div className="m-2">
+                <input
+                  type="radio"
+                  name="Gendar"
+                  id="Male"
+                  value="Male"
+                  onChange={() => setIsMale(true)}
+                  checked={isMale || false}
+                  />
+                <label htmlFor="Male">Male</label>
+              </div>
+              <div className="m-2">
+                <input
+                  type="radio"
+                  name="Gender"
+                  id="Female"
+                  value="Female"
+                  onChange={() => setIsMale(false)}
+                  checked={isMale == null || !isMale}
+                  />
+                <label htmlFor="Female">Female</label>
+              </div>
+            </fieldset>
+
+            <div className="flex flex-col">
+              <label 
+                htmlFor="weight"
+                className="font-medium p-2 py-3"
+              >
+                Weight in KG
+              </label>
               <input
-                type="radio"
-                name="Gendar"
-                id="Male"
-                value="Male"
-                onChange={() => setIsMale(true)}
-                checked={isMale || false}
-              />
-              <label htmlFor="Male">Male</label>
+                className="border-solid border-2 border-gray-700 rounded-sm p-1 m-2"
+                type="number"
+                name="weight"
+                id="weight"
+                value={weight ?? ""}
+                onChange={handleWeightChange}
+                />
             </div>
-            <div>
+
+          </div>
+
+          <fieldset>
+            <legend className="text-center font-medium py-3">Alcohol</legend>
+
+            <div className="flex flex-col">
+              <label htmlFor="ml">Volume in ml</label>
               <input
-                type="radio"
-                name="Gender"
-                id="Female"
-                value="Female"
-                onChange={() => setIsMale(false)}
-                checked={isMale == null || !isMale}
+                className="border-solid border-2 border-gray-700 round-sm p-1 m-2"
+                type="number"
+                name="ml"
+                id="ml"
+                value={ml ?? ""}
+                onChange={handleMlChange}
               />
-              <label htmlFor="Female">Female</label>
+            </div>
+
+            <div className="flex flex-col">
+              <label htmlFor="ABV">ABV of this bevarage</label>
+              <input
+                className="border-solid border-2 border-gray-700 round-sm p-1 m-2"
+                type="number"
+                name="ABV"
+                id="ABV"
+                value={abv ?? ""}
+                onChange={handleAbvChange}
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label htmlFor="mins">Over how many Mins</label>
+              <input
+                className="border-solid border-2 border-gray-700 round-sm p-1 m-2"
+                type="number"
+                name="time"
+                id="mins"
+                value={time ?? ""}
+                onChange={handleTimeChange}
+              />
             </div>
           </fieldset>
-
-          <input
-            type="number"
-            name="weight"
-            id="weight"
-            value={weight ?? ""}
-            onChange={handleWeightChange}
-          />
-          <label htmlFor="weight">weight</label>
-
         </form>
+
+        {weight && isMale != null  && time && ml && abv && (
+          <div>
+            <p>
+              Your BAC would be:{" "}
+              {`${calcBAC({
+                alcholConsumed: calcAlcoholInGrams(ml, abv),
+                isMale: isMale,
+                weightInKG: weight,
+                timeInHours: time,
+              })}`}
+            </p>
+          </div>
+        )}
       </main>
     </>
   );
